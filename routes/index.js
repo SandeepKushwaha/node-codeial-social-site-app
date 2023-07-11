4a063d580c07dfaa5d667733a91db4d1df912dd3
SandeepKushwaha <sandeepkush327@gmail.com>
2023-07-11 17:20:20 +0530

    added users Controller and router

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controllers/home_controller');
-const userController = require('../controllers/user_controller');
 
 // router configuration
 router.get('/', homeController.home);
-router.get('/users', userController.user);
+router.use('', require('./users'));
 
 
 
---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+const userController = require('../controllers/user_controller');
+
+// router configuration
+router.get('/users', userController.user);
+
+module.exports = router;
