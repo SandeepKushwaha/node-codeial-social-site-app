5412870ce6205838e96013e64b32bfb29bf39ba8
SandeepKushwaha <sandeepkush327@gmail.com>
2023-07-14 18:13:52 +0530

    set up static files access

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-
 const expressLayouts = require('express-ejs-layouts');
+
+// set up static file access
+app.use(express.static('./assets'));
 
 app.use(expressLayouts);
 
