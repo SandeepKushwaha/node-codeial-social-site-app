c66cdab32a1a51149e7acce2ba4fe41c28d36b1d
SandeepKushwaha <sandeepkush327@gmail.com>
2023-07-15 16:01:10 +0530

    accessing cookie for home request

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,9 @@
 module.exports.home = function (request, response) { 
     // return response.end('<h1>Express is up for Codeial!</h1>');
+    console.log('cookies for home controller::', request.cookies);
+    response.cookie('user_id', 100);
 
     return response.render('home', {
-        title: 'Home',
+        title: 'Codeial | Home',
     });
 };
