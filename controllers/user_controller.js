b044f6b7d94e3de91f19b99a84a5fa07fa450f2f
SandeepKushwaha <sandeepkush327@gmail.com>
2023-07-15 14:47:09 +0530

    creating and rendering signup and signin pages

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,3 +3,29 @@ module.exports.user = function (request, response) {
         title: 'User Profile',
     });
 };
+
+// adding some action
+
+// render the sign up page
+module.exports.signUp = function (request, response) { 
+    return response.render('user_sign_up', {
+        title: 'Codeial | Sign Up',
+    });
+};
+
+// render the sign in page
+module.exports.signIn = function (request, response) { 
+    return response.render('user_sign_in', {
+        title: 'Codeial | Sign In',
+    });
+};
+
+// get the sign up data by post http action method
+module.exports.create = function (request, response) { 
+    // TODO implement your code here...
+};
+
+// sign in and create a session for the user
+module.exports.createSession = function (request, response) { 
+    // TODO implement your code here...
+};
--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,9 @@ const userController = require('../controllers/user_controller');
 // router configuration
 router.get('/profile', userController.user);
 
+// user signup 
+router.get('/sign-up', userController.signUp);
+// user signup 
+router.get('/sign-in', userController.signIn);
+
 module.exports = router;
