eb77c8ec1d0cd32d92112f40c445bafa465f9ebb
SandeepKushwaha <sandeepkush327@gmail.com>
2023-07-30 23:28:59 +0530

    Converting to Async Await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,9 +2,38 @@ const Post = require('../models/post');
 
 const User = require('../models/user');
 
+// async and await based example
+module.exports.home = async function (request, response) { 
+    response.cookie('user_id', request.cookies.user_id);
+
+    try { 
+        let posts = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user',
+            }
+        });
+    
+        let users = await User.find({});
+
+        return response.render('home', {
+            title: 'Codeial | Home',
+            posts: posts,
+            all_users: users,
+        });
+    } catch (error) { 
+        console.log('Error in fetching posts:', error);
+        return;
+    }
+};
+
+// promise based example 
+/*
 module.exports.home = function (request, response) { 
-    // return response.end('<h1>Express is up for Codeial!</h1>');
-    // console.log('cookies for home controller::', request.cookies);
+    /// return response.end('<h1>Express is up for Codeial!</h1>');
+    /// console.log('cookies for home controller::', request.cookies);
     response.cookie('user_id', request.cookies.user_id);
 
     Post.find({})
@@ -31,6 +60,7 @@ module.exports.home = function (request, response) {
         })
         .catch(error => {
             console.log('Error in fetching posts:', error);
-            // Handle the error, e.g., render an error page or redirect
+            /// Handle the error, e.g., render an error page or redirect
         });
 };
+*/
